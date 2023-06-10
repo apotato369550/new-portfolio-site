030f645f83ce65f893cc0d4cf871b3441c23aea7
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2023-06-10 13:02:22 +0800

    Create navbar

--- responsive_vite_react_header/src/components/Header.jsx
+++ responsive_vite_react_header/src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
                     <img src={close} width="100" height="100" className="close" title="close" />
                 </label>
                 <ul className="nav-menu">
-                    <li><a href="index.html" className="logo">JAY</a></li>
+                    <a href="index.html" className="logo">JAY</a>
                     <li><a href="index.html">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Projects</a></li>
