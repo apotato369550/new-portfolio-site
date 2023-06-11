1b751ec2b665cf11beffd2c327083d3c1bd8d3be
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2023-06-11 21:50:29 +0800

    Added 'Home' and 'ContactForm' components in responsive container

--- responsive_vite_contact_form/src/App.tsx
+++ responsive_vite_contact_form/src/App.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from "./components/Header"
+import Home from "./components/Home"
+import ContactForm from './components/ContactForm'
 
 function App() {
 
   return (
     <>
       <Header />
+        <div className="container">
+        <Home />
+        <ContactForm />
+      </div>
     </>
   )
 }
