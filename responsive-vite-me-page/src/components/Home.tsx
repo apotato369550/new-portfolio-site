01ed553a165c6936cd6eee59b06ac0b296aa173c
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2023-07-04 17:45:39 +0800

    Created initial files

---
+++ responsive-vite-me-page/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import './css/Home.css'
+
+function Home() {
+    return (
+        <section className="home">
+            {/* do stuff here */}
+        </section>
+    )
+}
+
+export default Home
